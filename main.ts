8f1e0a7828fa747f791dccc34928b03a631373a3
Monaka-1118 <164736018+Monaka-1118@users.noreply.github.com>
2024-03-25 00:07:17 +0900

    LED点灯機能追加

--- main.ts
+++ main.ts
@@ -3,21 +3,30 @@ radio.onReceivedValue(function (name, value) {
         Arm_Angle = value
     } else if (name == "Boom") {
         Boom_Angle = value
-    } else if (name == "senkai") {
-        senkai_Angle = value
-    } else {
-        pen = value
+    } else if (name == "pen") {
+        if (value == 0) {
+            red = 0
+            green = 0
+            blue = 0
+        } else if (value == 1) {
+            red = randint(0, 255)
+            green = randint(0, 255)
+            blue = randint(0, 255)
+        }
     }
 })
-let pen = 0
-let senkai_Angle = 0
+let blue = 0
+let green = 0
+let red = 0
 let Boom_Angle = 0
 let Arm_Angle = 0
 radio.setGroup(1)
 Arm_Angle = 1450
 Boom_Angle = 1450
-senkai_Angle = 90
-pen = 90
+let pen = 0
+red = 0
+green = 0
+blue = 0
 basic.showLeds(`
     . . . . .
     . . # . .
@@ -28,5 +37,7 @@ basic.showLeds(`
 basic.forever(function () {
     pins.servoSetPulse(AnalogPin.P12, Arm_Angle)
     pins.servoSetPulse(AnalogPin.P16, Boom_Angle)
-    pins.servoWritePin(AnalogPin.P11, pen)
+    pins.analogWritePin(AnalogPin.P0, red)
+    pins.analogWritePin(AnalogPin.P1, green)
+    pins.analogWritePin(AnalogPin.P2, blue)
 })
