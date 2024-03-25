da7f5920b24a5ffee703f55c739032a8aa129162
Monaka-1118 <164736018+Monaka-1118@users.noreply.github.com>
2024-03-26 01:35:07 +0900

    LEDをアナログ出力するとサーボにノイズが乗るため、デジタル出力に変更

--- main.ts
+++ main.ts
@@ -4,29 +4,19 @@ radio.onReceivedValue(function (name, value) {
     } else if (name == "Boom") {
         Boom_Angle = value
     } else if (name == "pen") {
-        if (value == 0) {
-            red = 0
-            green = 0
-            blue = 0
-        } else if (value == 1) {
-            red = randint(0, 255)
-            green = randint(0, 255)
-            blue = randint(0, 255)
-        }
+        pen = value
     }
 })
-let blue = 0
-let green = 0
-let red = 0
+let pen = 0
 let Boom_Angle = 0
 let Arm_Angle = 0
 radio.setGroup(1)
 Arm_Angle = 1450
 Boom_Angle = 1450
-let pen = 0
-red = 0
-green = 0
-blue = 0
+pen = 0
+let red = 255
+let green = 0
+let blue = 0
 basic.showLeds(`
     . . . . .
     . . # . .
@@ -35,9 +25,16 @@ basic.showLeds(`
     . . . . .
     `)
 basic.forever(function () {
+	
+})
+basic.forever(function () {
     pins.servoSetPulse(AnalogPin.P12, Arm_Angle)
     pins.servoSetPulse(AnalogPin.P16, Boom_Angle)
-    pins.analogWritePin(AnalogPin.P0, red)
-    pins.analogWritePin(AnalogPin.P1, green)
-    pins.analogWritePin(AnalogPin.P2, blue)
+    if (pen == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
 })
